1e6467ab5dd1c7e24bd090f37539e8ed6b764de2
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-05-30 22:01:54 -0300

    [Frontend_NPM_Gulp] Browser sync 2

--- frontend_npm_gulp/gulp/gulpfile.js
+++ frontend_npm_gulp/gulp/gulpfile.js
@@ -1,23 +1,28 @@
+// Node modules
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
-function compilaSass() {
+// Function for sass compiling and autoprefixing
+function compileSass() {
   return gulp
-    .src('css/scss/*.scss')
-    .pipe(sass({ 
-      outputStyle: 'compressed' 
-    }))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }))
-    .pipe(gulp.dest('css/'))
+  .src('css/scss/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.stream());
 }
 
-gulp.task('sass', compilaSass);
+// Gulp task for sass compilation
+gulp.task('sass', compileSass);
 
+// Init browser-sync lib
 function browser() {
   browserSync.init({
     server: {
@@ -26,13 +31,17 @@ function browser() {
   });
 }
 
+// Init browser-sync task
 gulp.task('browser-sync', browser);
 
+// Watching for changes in html and scss files
 function watch() {
-  gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch('css/scss/*.scss', compileSass);
+  gulp.watch(['*.html']).on('change', browserSync.reload);
 }
 
-
+// Init watch task
 gulp.task('watch', watch);
 
+// Gulp's default task which inits watch and browser-sync
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
