4f7df96ede4545ab600ff61476efb0729fbac9f3
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-05-30 21:28:09 -0300

    [Frontend_NPM_Gulp] Gulp autoprefixer

--- frontend_npm_gulp/gulp/gulpfile.js
+++ frontend_npm_gulp/gulp/gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass() {
   return gulp
     .src('css/scss/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sass({ 
+      outputStyle: 'compressed' 
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulp.dest('css/'))
 }
 
